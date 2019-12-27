94fdcf79935a9f91747ede28a393eacee6171d37
Majid Qureshi <mpq1990@gmail.com>
2019-12-28 00:00:05 +0100

    feat: add showcase of using env variable directly

--- src/main/java/com/example/dispring/config/PropertyConfig.java
+++ src/main/java/com/example/dispring/config/PropertyConfig.java
@@ -1,11 +1,13 @@
 package com.example.dispring.config;
 
 import com.example.dispring.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
@@ -20,13 +22,15 @@ public class PropertyConfig {
     @Value("${guru.dburl}")
     String url;
 
+    @Autowired
+    Environment env;
 
     @Bean
     public FakeDataSource fakeDataSource() {
         FakeDataSource fakeDataSource = new FakeDataSource();
         fakeDataSource.setUser(user);
         fakeDataSource.setPassword(password);
-        fakeDataSource.setUrl(url);
+        fakeDataSource.setUrl(env.getProperty("DBURL"));
         return fakeDataSource;
     }
 
