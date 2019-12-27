1503bbf77f5874575091aa643074de948c85ac65
Majid Qureshi <mpq1990@gmail.com>
2019-12-27 23:48:46 +0100

    feat: add property source to fetch external properties

--- src/main/java/com/example/dispring/DiSpringApplication.java
+++ src/main/java/com/example/dispring/DiSpringApplication.java
@@ -4,6 +4,7 @@ import com.example.dispring.controllers.ConstructorInjectedController;
 import com.example.dispring.controllers.MyController;
 import com.example.dispring.controllers.PropertyInjectedController;
 import com.example.dispring.controllers.SetterInjectedController;
+import com.example.dispring.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -15,11 +16,16 @@ public class DiSpringApplication {
         ApplicationContext ctx = SpringApplication.run(DiSpringApplication.class, args);
         MyController myController = (MyController) ctx.getBean("myController");
 
-        System.out.println(myController.hello());
+//        System.out.println(myController.hello());
+//
+//        System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
+//        System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
+//        System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
 
-        System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
-        System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
-        System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+        FakeDataSource fakeDataSource = ctx.getBean(FakeDataSource.class);
+        System.out.println(fakeDataSource.getUser());
+        System.out.println(fakeDataSource.getPassword());
+        System.out.println(fakeDataSource.getUrl());
     }
 
 }
